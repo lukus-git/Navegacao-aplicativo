b8ffc7324c918b69a68cb5b447afc5afbfc057cf
lukus-git <lucas.garbi13@hotmail.com>
2025-10-31 20:31:00 -0300

    adicionando função de calculalar IMC

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native'
 
--- Screens/IMCScreen.js
+++ Screens/IMCScreen.js
@@ -1,10 +1,62 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { useState } from 'react';
 
-export default function IMCScreen({ navigation }) {
+export default function IMCScreen() {
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+    const [resultado, setResultado] = useState('');
+
+    const calcula = () => {
+        const pesoPessoa = parseFloat(peso);
+        const alturaPessoa = parseFloat(altura);
+        const imc = pesoPessoa / (alturaPessoa * alturaPessoa);
+        setResultado(imc);
+
+        if (imc < 20.7) {
+            return imc + "Abaixo do peso";
+        }
+        if (imc < 26.4) {
+            return imc + "Peso ideal";
+        }
+        if (imc < 27.8) {
+            return imc + "Levemente acima do peso";
+        }
+        if (imc < 31.1) {
+            return imc + "obesidade";
+        } else {
+            return imc + "vc esta imensa";
+        }
+    };
+
+
+
     return (
         <View style={estilos.container}>
             <Text style={estilos.texto}>Calculadora de IMC.</Text>
+
+            <TextInput
+                style={estilos.input}
+                placeholder="Digite seu peso"
+                value={peso}
+                onChangeText={setPeso}
+            />
+
+            <TextInput
+                style={estilos.input}
+                placeholder="Digite sua altura"
+                value={altura}
+                onChangeText={setAltura}
+            />
+
+            <View style={estilos.botao}>
+                <Button title="calcular IMC" onPress={calcula} />
+            </View>
+
+            <Text style={estilos.texto}>Resultado é {resultado}</Text>
+
         </View>
+
+
     );
 }
 
@@ -19,11 +71,20 @@ const estilos = StyleSheet.create({
     },
     botao: {
         width: '60%',
-        marginVertical: 20,
+        marginVertical: 10,
     },
     texto: {
         fontFamily: 'Arial',
         fontSize: 18,
         marginBottom: 10,
+    },
+    input: {
+        backgroundColor: 'white',
+        marginVertical: 10,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'gray',
+        width: '80%',
+        paddingHorizontal: 10,
     },
 });
